a62f19ced2d0ede317e334b5cb6f3bb914ad8450
Henrick Stafford <henrick.stafford@outlook.com>
2022-01-05 01:20:34 -0600

    Rebase

--- Index.js
+++ Index.js
@@ -1,14 +1,49 @@
-const Discord = require('npm install discord.js');
- const client = new Discord.Client(909287648126980176);
+const config = require("./config.json");
+const token = require("./token.json");
+const Discord = require("discord.js");
+const fs = require("fs");
+const bot = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS"] });
+bot.commands = new Discord.Collection();
 
-client.on('ready', (HDahNTVX-5a_rjT302vTjk0IWI7EY--Z) => {
- console.log(`Logged in as ${client.user.tag}!`);
- });
+const commandFiles = fs.readdirSync('./commands/').filter(f => f.endsWith('.js'))
+for (const file of commandFiles) {
+    const props = require(`./commands/${file}`)
+    console.log(`${file} loaded`)
+    bot.commands.set(props.help.name, props)
+}
 
-client.on('message', msg => {
- if (msg.content === 'ping') {
- msg.reply('pong');
- }
- });
+// Load Event files from events folder
+const eventFiles = fs.readdirSync('./events/').filter(f => f.endsWith('.js'))
 
-client.login('OTA5Mjg3NjQ4MTI2OTgwMTc2.YZCGHQ.FCvsh38HVLPtR_qaftve3rltMB4');
+for (const file of eventFiles) {
+    const event = require(`./events/${file}`)
+    if(event.once) {
+        bot.once(event.name, (...args) => event.execute(...args, bot))
+    } else {
+        bot.on(event.name, (...args) => event.execute(...args, bot))
+    }
+}
+
+//Command Manager
+bot.on("message", async message => {
+    //Check if author is a bot or the message was sent in dms and return
+    if(message.author.bot) return;
+    if(message.channel.type === "dm") return;
+
+    //get prefix from config and prepare message so it can be read as a command
+    let prefix = config.prefix;
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+
+    //Check for prefix
+    if(!cmd.startsWith(config.prefix)) return;
+
+    //Get the command from the commands collection and then if the command is found run the command file
+    let commandfile = bot.commands.get(cmd.slice(prefix.length));
+    if(commandfile) commandfile.run(bot,message,args);
+
+});
+
+//Token needed in token.json
+bot.login(token.token);
