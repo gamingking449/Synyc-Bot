6955ce15cb6ca6d0a821e3cd7e7acf7ef87d68df
gamingking449 <noahfarguson6@gmail.com>
2022-01-05 01:08:54 -0600

    Bots

---
+++ Index.js
@@ -0,0 +1,14 @@
+const Discord = require('npm install discord.js');
+ const client = new Discord.Client(909287648126980176);
+
+client.on('ready', (HDahNTVX-5a_rjT302vTjk0IWI7EY--Z) => {
+ console.log(`Logged in as ${client.user.tag}!`);
+ });
+
+client.on('message', msg => {
+ if (msg.content === 'ping') {
+ msg.reply('pong');
+ }
+ });
+
+client.login('OTA5Mjg3NjQ4MTI2OTgwMTc2.YZCGHQ.FCvsh38HVLPtR_qaftve3rltMB4');
